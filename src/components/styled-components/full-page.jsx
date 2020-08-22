572e17d72b8f25836700236b7278544b07964897
Damon Roberts <66637938+damonspencerroberts@users.noreply.github.com>
2020-08-21 23:36:41 -0400

    Added Diagonal BG color

---
+++ src/components/styled-components/full-page.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const BackgroundDiv = styled.div`
+    background-color: #013A6B;
+    background-image: -webkit-linear-gradient(30deg, #013A6B 50%, #004E95 50%);
+    min-height: 500px;
+`;
+
+export {BackgroundDiv};
