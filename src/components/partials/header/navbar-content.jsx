ca7481886b6ab329706e79a5a2fdb992062628e6
Damon Roberts <66637938+damonspencerroberts@users.noreply.github.com>
2020-08-24 18:21:29 -0400

    Fixed burger menu.

--- src/components/partials/header/navbar-content.jsx
+++ src/components/partials/header/navbar-content.jsx
@@ -7,25 +7,25 @@ import NavDrop from "./nav-dropdown";
 import drsicon from "../../img/drsicon.png";
 
 const navbar = () => 
-<Navbar collapseOnSelect  expand="lg" className = "color-nav" variant = "light">
-  <Navbar.Brand href="/">
-    <img
-        alt="drsicon"
-        src= {drsicon}
-        width="80"
-        height="80"
-        className="d-inline-block align-top"
-        />{' '}
-</Navbar.Brand>
-<Nav className="ml-auto">
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Nav.Link href="/"><i className="fas fa-home"></i> Home</Nav.Link>
-            <Nav.Link href={"/" + _.lowerCase("education")}><i className="fas fa-university"></i> Education</Nav.Link>
-            <Nav.Link href={"/" + _.lowerCase("contact")}><i className="fas fa-envelope"></i> Contact</Nav.Link>
-            <NavDrop />
-        </Navbar.Collapse>
-    </Nav>
-  </Navbar>
+  <Navbar  expand="lg" className = "color-nav" variant = "light">
+		<Navbar.Brand href="/">
+			<img
+				alt="drsicon"
+				src= {drsicon}
+				width="80"
+				height="80"
+				className="d-inline-block align-top"
+				/>
+		</Navbar.Brand>
+		<Navbar.Toggle aria-controls="basic-navbar-nav" />
+		<Navbar.Collapse id="basic-navbar-nav">
+			<Nav className="ml-auto">
+				<Nav.Link href="/"><i className="fas fa-home"></i> Home</Nav.Link>
+				<Nav.Link href={"/" + _.lowerCase("education")}><i className="fas fa-university"></i> Education</Nav.Link>
+				<Nav.Link href={"/" + _.lowerCase("contact")}><i className="fas fa-envelope"></i> Contact</Nav.Link>
+				<NavDrop />
+			</Nav>
+		</Navbar.Collapse>
+    </Navbar>
 
 export default navbar;
